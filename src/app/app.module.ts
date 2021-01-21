c87438707c3f687122a77c9ad14f397f2d4a4c19
Vitaliy5 <dydvitrom@ukr.net>
2021-01-21 11:46:02 +0200

    add auction page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuctionsPageComponent } from './auctions-page/auctions-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuctionsPageComponent
   ],
   imports: [
     BrowserModule,
