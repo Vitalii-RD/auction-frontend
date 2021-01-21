6666a74d6687caa556c9d8562c050aa87259cace
Vitaliy5 <dydvitrom@ukr.net>
2021-01-21 23:21:30 +0200

    add auction page layout and logic for get all auctions

--- src/app/auctions-page/auctions-page.component.ts
+++ src/app/auctions-page/auctions-page.component.ts
@@ -1,4 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import Auction from 'src/types/Auction';
+import { AuctionService } from './auction-page.service';
 
 @Component({
   selector: 'app-auctions-page',
@@ -6,10 +10,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auctions-page.component.scss']
 })
 export class AuctionsPageComponent implements OnInit {
+  auctions: Auction[];
+  isModal: boolean = false;
+  error: String = '';
+  
+  defaultForm = {
+    title: '',
+    initialBid: ''
+  }
+  
+  auctionForm =  this.formBuilder.group(this.defaultForm);
 
-  constructor() { }
+  constructor(
+    private auctionService: AuctionService,
+    private formBuilder: FormBuilder
+    ) {
+    this.auctions = []
+  }
 
   ngOnInit(): void {
+    this.getAuctions();
+  }
+
+  getAuctions() {
+    this.auctionService.getAuctions()
+    .subscribe(
+      (data:Auction[]) => {
+        this.auctions = data
+        console.log(data);
+      },
+      (e:HttpErrorResponse) => {
+        this.auctions = [];
+        this.error = "Oops, something went wrong. Can not load the tasks.";
+      } 
+      );
+  }
+
+  closeModal() {
+    this.isModal = false;
+  }
+
+  openModal() {
+    this.isModal = true;
+  }
+
+  onSubmit() {
+    this.closeModal();
+    this.auctionForm.reset(this.defaultForm);
   }
 
 }
