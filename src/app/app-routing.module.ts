97c3efc1912e164bd2e9f299690c508208632830
Vitaliy5 <dydvitrom@ukr.net>
2021-01-21 16:32:38 +0200

    add auction card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuctionsPageComponent } from './auctions-page/auctions-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'auctions', component: AuctionsPageComponent },
+  { path: '', redirectTo: 'auctions', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
